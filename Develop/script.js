a92cb09707ec905ced4f312f387f020b3e33158e
Elyse Wells <cewells89@gmail.com>
2020-10-06 22:33:30 -0400

    added logic to parse calendar hours into moment.js format

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 
     // Variable Declarations
 
-    var hourBlocks = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+    var hourBlocks = ["9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM"];
     var container = $(".container");
+    var currentTime = moment().format('HH');
+        console.log(currentTime);
 
     // Display Today's Date using Moment.js
     function getDate(){
@@ -17,29 +19,33 @@ $(document).ready(function(){
 
     // Create the Daily Planner with Office Hour Blocks
     function displayCalendar(){
-
         for(var i=0; i < hourBlocks.length; i++){
-            var elRow = $("<div>");
-            var elHour = $("<div>");
-            var elText = $("<textarea>");
-            var elSave = $("<div>");
+            var elRow = $("<div>").addClass("time-block row");
+            var elHour = $("<div>" + hourBlocks[i] + "</div>").addClass("hour col-md-1");
+            var elText = $("<textarea>").addClass("col-md-10");
+            var elSave = $("<div>").addClass("saveBtn col-md-1");
 
-            
-            elRow.attr("class", "time-block row");
-            elHour.attr("class","hour col-md-1");
-            elText.attr("class","col-md-10");
-            elSave.attr("class","saveBtn col-md-1");
-
-            elHour.text(hourBlocks[i]);
+            // Variable to parse the hourBlock array strings into moment.js format so they can be used for calculations
+            var parsedTime = moment(hourBlocks[i], "h a")
+                // console.log(parsedTime);
 
             container.append(elRow);
             elRow.append(elHour);
             elRow.append(elText);
             elRow.append(elSave);
-            
 
+            // Logic to Determine Formatting Based on Current Time
+            if(parsedTime >currentTime){
+                $(elText).attr("class","past col-md-10");
+            }else if(parsedTime === currentTime){
+                $(elText).attr("class", "current col-md-10");
+            }
+            else{
+                $(elText).attr("class","future col-md-10");
+            } 
         }   
     }
+
     getDate();
     displayCalendar();
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bootcamp-homeork-05-jquery-calendar
 Homework Assignment 4: jQuery Calendar
+
+
+#tutorials/resources
+* Moment.js: https://momentjs.com/
+* * Moment.js Math: https://momentjs.com/guides/#/lib-concepts/date-time-math/
+* * Moment.js Parsing: https://momentjs.com/guides/#/parsing/
+* * Moment.js parsing strings and assigning tokens to convert to moment format for calculations: https://momentjs.com/docs/#/parsing/string-format/
+* Bootstrap columns: https://getbootstrap.com/docs/4.5/layout/grid/
+* .addClass(): https://api.jquery.com/addclass/
+* Previous class activities for jQuery
